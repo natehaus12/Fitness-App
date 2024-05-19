8ed47bed7938014b76bc8e3227e21418fbc5d38a
Nate Hollingshaus <hol19020@byui.edu>
2024-05-19 17:29:08 -0600

    login functionality

--- virtualEnvironment/fitnessApp/fitnessApp/settings.py
+++ virtualEnvironment/fitnessApp/fitnessApp/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'fitnessApp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['tempates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- virtualEnvironment/fitnessApp/members/urls.py
+++ virtualEnvironment/fitnessApp/members/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('members/', views.members, name='members'),
+    path('', views.index, name='home'),
+    path('login/', views.user_login, name='login'),
+    path('signup/', views.user_signup, name='signup'),
+    path('logout/', views.user_logout, name='logout'),
 ]
