9efff80d18168adb26522c3af835aeaa3646a6f2
Nate Hollingshaus <hol19020@byui.edu>
2024-06-09 21:20:59 -0600

    fix log in

--- virtualEnvironment/fitnessApp/members/views.py
+++ virtualEnvironment/fitnessApp/members/views.py
@@ -1,11 +1,14 @@
 from django.http import HttpResponse
 from django.template import loader
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from .forms import SignupForm, LoginForm
 import requests
 from django.http import JsonResponse
 from . import forms
 from fatsecret import Fatsecret
 from . import models
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect
 
     
 def members(request):
@@ -42,6 +45,7 @@ def user_login(request):
             password = form.cleaned_data['password']
             user = authenticate(request, username=username, password=password)
             if user:  
+                login(request, user)
                 return redirect('home')
     else:
         form = LoginForm()
